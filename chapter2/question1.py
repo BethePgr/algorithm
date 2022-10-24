b8462b44cbc54bcf1a3985a32f3b4628e5d4b195
jongwonpc <c3392302@naver.com>
2022-10-25 01:10:27 +0900

    he

--- chapter2/question1.py
+++ chapter2/question1.py
@@ -1,14 +1,32 @@
 import sys
 sys.stdin = open("input.txt","r")
 n = int(input())
+# for i in range(n):
+#     s=input()
+#     s=s.upper()
+#     letter=""
+#     for i in s:
+#         letter = i + letter
+#     if s == letter:
+#         print("YES")
+#     else:
+#         print("NO")
+
+# for i in range(n):
+#     s= input()
+#     s=s.upper()
+#     size = len(s)
+#     for j in range(size//2):
+#         if s[j] != s[-j-1]:
+#             print("NO")
+#             break
+#     else:
+#         print("YES")
+
 for i in range(n):
     s=input()
     s=s.upper()
-    letter=""
-    for i in s:
-        letter = i + letter
-    if s == letter:
+    if s == s[::-1]:
         print("YES")
     else:
         print("NO")
-
