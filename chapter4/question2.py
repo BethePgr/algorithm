cbbfd83e8dcd4571b9c2966e7ee126cf69def1c8
jongwonpc <c3392302@naver.com>
2022-11-25 08:57:23 +0900

    he

--- chapter4/question2.py
+++ chapter4/question2.py
@@ -1,7 +1,16 @@
 import sys
-sys.stdin = open("input.txt","r")
+#sys.stdin = open("input.txt","r")
 s=input()
 stack=[]
+answer=0
 for i in range(len(s)):
     if s[i] == "(":
         stack.append(i)
+    elif s[i] == ")":
+        if s[i-1] == "(":
+            stack.pop()
+            answer+=len(stack)
+        elif s[i-1] == ")":
+            stack.pop()
+            answer+=1
+print(answer)
